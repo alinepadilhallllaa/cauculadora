43004e120a76920ca801d677388fdc04894e5b2c
john <jp.adada@live.com>
2019-04-08 13:54:08 -0300

    iniciando a classe

---
+++ calculadora/src/calculadora/calculadora.java
@@ -0,0 +1,16 @@
+package calculadora;
+import javax.swing.*;
+
+
+public class calculadora {
+   public static void main(String[] args) {
+      
+      String numA = JOptionPane.showInputDialog(null, "Digite o primeiro número:");
+      String numB = JOptionPane.showInputDialog(null, "Digite o segundo número:");
+      //transfomando os Strings em Inteiros
+      double a = Double.parseDouble(numA);
+      double b = Double.parseDouble(numB);
+      //calculando
+      double soma = a + b;
+   }
+}
